7cccafab79d201b9e10ba1b7953834ce8993366c
Justin Wheeler <wheelers_webistes@yahoo.com>
2021-04-16 23:45:00 -0400

    Generated Components for Resume Sections

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { CertificationsComponent } from './certifications/certifications.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExperienceComponent,
+    EducationComponent,
+    CertificationsComponent,
+    SkillsComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule
