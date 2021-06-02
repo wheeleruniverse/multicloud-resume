f96be16f4659c5b96ef8ee3bd59c88ead19855f0
Justin Wheeler <wheelers_webistes@yahoo.com>
2021-06-02 00:23:34 -0400

    Cookie Service for Visitor and UUID Package to Track Visitors Uniquely

--- web/src/app/visitor/visitor.component.ts
+++ web/src/app/visitor/visitor.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { VisitorService } from '../core/service/visitor/visitor.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { skipUntil, switchMap } from 'rxjs/operators';
+import { CookieService } from 'ngx-cookie-service';
+import { v4 } from 'uuid';
 
 @Component({
   selector: 'app-visitor',
@@ -10,6 +11,7 @@ import { skipUntil, switchMap } from 'rxjs/operators';
 })
 export class VisitorComponent implements OnInit {
   constructor(
+    private cookieService: CookieService,
     private service: VisitorService,
     private snackBar: MatSnackBar
   ) {}
@@ -17,24 +19,31 @@ export class VisitorComponent implements OnInit {
   total: number;
 
   ngOnInit(): void {
-    const config = { duration: 3000 };
-    const visitor = { name: window.navigator.userAgent };
-
-    this.service.create(visitor).subscribe((res) => {
-      this.initTotal();
-      if (res.status === 201) {
-        this.snackBar.open('Visitor Created!', null, config);
-        this.total += 1;
-      } else {
-        this.snackBar.open('Visitor Failure!', null, config);
-      }
-    });
-
     this.service.count().subscribe((count) => {
       this.initTotal();
       count.forEach((c) => {
         this.total += c.cnt;
       });
+    });
+
+    if (!this.cookieService.check('Visitor')) {
+      this.createVisitor();
+    }
+  }
+
+  private createVisitor(): void {
+    const config = { duration: 3000 };
+    const visitor = { id: v4(), name: window.navigator.userAgent };
+
+    this.service.create(visitor).subscribe((res) => {
+      this.initTotal();
+      if (res.status === 201) {
+        this.cookieService.set('Visitor', visitor.id, 30);
+        this.snackBar.open('Visitor Created!', null, config);
+        this.total += 1;
+      } else {
+        this.snackBar.open('Visitor Failure!', null, config);
+      }
     });
   }
 
