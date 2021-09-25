e732a0377052bfaca3b83aa878d4ec811efb1f3f
Justin Wheeler <wheelers_webistes@yahoo.com>
2021-09-25 12:00:00 -0400

    Initial App Commit (Python/Flask/Docker), Initial IaC Commit (Terraform)

---
+++ iac/resources.tf
@@ -0,0 +1,101 @@
+
+resource "google_app_engine_application" "this" {
+  /*
+   * Note:
+   *
+   * Two locations, which are called europe-west and us-central in App Engine commands and in the Google Cloud Console,
+   * are called europe-west1 and us-central1, respectively, elsewhere in Google documentation.
+   *
+   * https://cloud.google.com/appengine/docs/locations
+   */
+  database_type = "CLOUD_FIRESTORE"
+  location_id   = "US-CENTRAL1" == var.location_region ? "us-central" : lower(var.location_region)
+}
+
+resource "google_cloud_run_domain_mapping" "this" {
+  location = google_cloud_run_service.this.location
+  name     = "api.${var.fqdn}"
+
+  metadata {
+    namespace = var.project
+  }
+  spec {
+    route_name = google_cloud_run_service.this.name
+  }
+}
+
+resource "google_cloud_run_service" "this" {
+  autogenerate_revision_name = true
+  location                   = lower(var.location_region)
+  name                       = var.domain
+
+  template {
+    metadata {
+      labels = local.labels
+    }
+    spec {
+      containers {
+        image = var.image
+
+        env {
+          name  = "GCP_CREDENTIALS"
+          value = var.service_account_json
+        }
+      }
+    }
+  }
+  traffic {
+    latest_revision = true
+    percent         = 100
+  }
+}
+
+resource "google_compute_backend_bucket" "this" {
+  bucket_name = google_storage_bucket.this.name
+  enable_cdn  = true
+  name        = google_storage_bucket.this.name
+}
+
+resource "google_compute_global_forwarding_rule" "this" {
+  name       = var.domain
+  target     = google_compute_target_https_proxy.this.id
+  port_range = "443"
+}
+
+resource "google_compute_managed_ssl_certificate" "this" {
+  name = var.domain
+
+  managed {
+    domains = [
+      "www.${var.fqdn}."
+    ]
+  }
+}
+
+resource "google_compute_target_https_proxy" "this" {
+  name             = var.domain
+  ssl_certificates = [google_compute_managed_ssl_certificate.this.id]
+  url_map          = google_compute_url_map.this.id
+}
+
+resource "google_compute_url_map" "this" {
+  default_service = google_compute_backend_bucket.this.id
+  name            = var.domain
+}
+
+resource "google_container_registry" "this" {
+  location = var.location_multi_region
+}
+
+resource "google_storage_bucket" "this" {
+  name                        = var.domain
+  force_destroy               = true
+  labels                      = local.labels
+  location                    = var.location_multi_region
+  storage_class               = var.storage_class
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+  }
+}
---
+++ iac/variables.tf
@@ -0,0 +1,80 @@
+
+locals {
+  labels = {
+    environment = var.environment
+    project     = var.project
+  }
+}
+
+variable "domain" {
+  default     = "xxxxxx"
+  description = "domain name"
+  type        = string
+}
+
+variable "environment" {
+  default     = "prd"
+  description = "environment name"
+  type        = string
+}
+
+variable "location_dual_region" {
+  default     = "NAM4"
+  description = "a specific pair of regions, such as Tokyo and Osaka"
+  type        = string
+
+  validation {
+    condition     = contains(["NAM4"], var.location_dual_region)
+    error_message = "Variable location_dual_region is invalid."
+  }
+}
+
+variable "location_multi_region" {
+  default     = "US"
+  description = "a large geographic area, such as the United States, that contains two or more geographic places"
+  type        = string
+
+  validation {
+    condition     = contains(["US"], var.location_multi_region)
+    error_message = "Variable location_multi_region is invalid."
+  }
+}
+
+variable "location_region" {
+  default     = "US-CENTRAL1"
+  description = "a specific geographic place, such as São Paulo"
+  type        = string
+
+  validation {
+    condition     = contains(["US-CENTRAL1", "US-EAST1", "US-EAST4", "US-WEST1", "US-WEST2", "US-WEST3", "US-WEST4"], var.location_region)
+    error_message = "Variable location_region is invalid."
+  }
+}
+
+variable "fqdn" {
+  default     = "xxxxxx.com"
+  description = "fully qualified domain name"
+  type        = string
+}
+
+variable "image" {
+  description = "container image"
+  type        = string
+}
+
+variable "project" {
+  default     = "cloudguruchallenge-2108"
+  description = "project name"
+  type        = string
+}
+
+variable "service_account_json" {
+  description = "service account json"
+  type        = string
+}
+
+variable "storage_class" {
+  default     = "STANDARD"
+  description = "storage class"
+  type        = string
+}
