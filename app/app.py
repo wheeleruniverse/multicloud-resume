d748e16985f5d6d40d72a6f5f3e8945d6a81e50a
Justin Wheeler <wheelers_webistes@yahoo.com>
2021-09-26 11:20:38 -0400

    Add gunicorn to the Flask App

--- app/app.py
+++ app/app.py
@@ -3,5 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def main():
     return 'Hello, Docker!'
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
