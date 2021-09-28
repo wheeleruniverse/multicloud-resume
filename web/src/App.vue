6656fd13ffcaf550be7c3954df8b970013ad49e1
Justin Wheeler <wheelers_webistes@yahoo.com>
2021-09-28 03:08:49 -0400

    Changes for Web: Added Resume Component, Removed HelloWorld Component

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Resume
+      header="Vue Resume Sample"
+      description="This is a sample application created in Vue for the sole purpose of exploring GCP services that I
+      would not use in my actual Resume website. I refused to use these GCP services like Cloud Build and Cloud Source
+      Repositories in that actual code simply because I am already using another technology. My official Resume is
+      hosted on three cloud providers (AWS, Azure, and GCP) so I didn't think it was wise to use services exclusive to
+      GCP for multiple cloud providers. Lastly, I didn't want to implement multiple solutions that accomplish the same
+      goal."
+      resumeUrl="https://gcp.wheelercloudguru.com"
+      visitorCountApi="https://api.wheelersadvice.com/visitor/count"
+      visitorIncrementApi="https://api.wheelersadvice.com/visitor/increment"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Resume from './components/Resume.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Resume
   }
 }
 </script>
---
+++ web/src/components/Resume.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="resume">
+    <div class="section">
+      <h1 class="header">
+        {{ header }}
+      </h1>
+      <p class="description">
+        {{description}}
+      </p>
+    </div>
+    <div class="section">
+      <a v-bind:href="resumeUrl" class="resume-link">Resume</a>
+    </div>
+    <div class="section">
+      <a v-bind:href="visitorCountApi">{{visitorCountApi}}</a>
+    </div>
+    <div class="section">
+      <a v-bind:href="visitorIncrementApi">{{visitorIncrementApi}}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Resume',
+  props: {
+    header: String,
+    description: String,
+    resumeUrl: String,
+    visitorCountApi: String,
+    visitorIncrementApi: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.description {
+  line-height: 2;
+  margin: 0 auto;
+  width: 70%;
+}
+
+.header {
+  color: #5555aa;
+}
+
+.resume {
+  display: flex;
+  flex-direction: column;
+}
+
+.resume-link {
+  background-color: #5555aa;
+  border: 2px solid black;
+  border-radius: 8px;
+  color: white;
+  padding: 8px;
+  text-decoration: none;
+}
+
+.section {
+  margin: 16px auto;
+}
+
+</style>
