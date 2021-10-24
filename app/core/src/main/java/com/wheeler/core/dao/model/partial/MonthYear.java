543425641fe530b7a56c8bf8fc0f594e1f29659a
Justin Wheeler <wheelers_webistes@yahoo.com>
2021-10-24 17:39:28 -0400

    app/core MonthYear

--- app/core/src/main/java/com/wheeler/core/dao/model/partial/MonthYear.java
+++ app/core/src/main/java/com/wheeler/core/dao/model/partial/MonthYear.java
@@ -2,9 +2,11 @@ package com.wheeler.core.dao.model.partial;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class MonthYear {
 
     private Integer month;
